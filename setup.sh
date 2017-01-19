f907b92f0d1de88a32a61ddc4fdd1298d860cf0a
techgaun <coolsamar207@gmail.com>
2017-01-18 20:12:43 -0600

    add spf13 vim install

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ ln -sf "${SRC_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
 ln -sf "${SRC_DIR}/.bash_aliases" "${HOME}/.bash_aliases"
 ln -sf "${SRC_DIR}/.vimrc.local" "${HOME}/.vimrc.local"
 ln -sf "${SRC_DIR}/.vimrc.bundles.local" "${HOME}/.vimrc.bundles.local"
+
+# setup spf13-vim if not installed already or FORCE_VIMINSTALL is set
+if [[ ! -d "${HOME}/.spf13-vim-3" || "x${FORCE_VIMINSTALL}" != "x" ]]; then
+  curl -L -o - https://github.com/techgaun/spf13-vim/raw/3.0/bootstrap.sh | sh
+fi
