6eee465c3d4ef6e26122f625a9cb0ec439fdaa90
techgaun <coolsamar207@gmail.com>
2017-01-19 12:41:17 -0600

    add ackrc

--- setup.sh
+++ setup.sh
@@ -8,14 +8,15 @@ if [[ ! -d "${HOME}/.spf13-vim-3" || "x${FORCE_VIMINSTALL}" != "x" ]]; then
     curl -L -o - https://github.com/techgaun/spf13-vim/raw/3.0/bootstrap.sh | sh
 fi
 
-ln -sf "${SRC_DIR}/.gitignore" "${HOME}/.gitignore"
-ln -sf "${SRC_DIR}/.gitconfig" "${HOME}/.gitconfig"
-ln -sf "${SRC_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${SRC_DIR}/.ackrc" "${HOME}/.ackrc"
 ln -sf "${SRC_DIR}/.bash_aliases" "${HOME}/.bash_aliases"
-ln -sf "${SRC_DIR}/.vimrc.local" "${HOME}/.vimrc.local"
-ln -sf "${SRC_DIR}/.vimrc.bundles.local" "${HOME}/.vimrc.bundles.local"
-ln -sf "${SRC_DIR}/.bashrc.defaults" "${HOME}/.bashrc.defaults"
 ln -sf "${SRC_DIR}/.bash_prompt" "${HOME}/.bash_prompt"
+ln -sf "${SRC_DIR}/.bashrc.defaults" "${HOME}/.bashrc.defaults"
+ln -sf "${SRC_DIR}/.gitconfig" "${HOME}/.gitconfig"
+ln -sf "${SRC_DIR}/.gitignore" "${HOME}/.gitignore"
+ln -sf "${SRC_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${SRC_DIR}/.vimrc.bundles.local" "${HOME}/.vimrc.bundles.local"
+ln -sf "${SRC_DIR}/.vimrc.local" "${HOME}/.vimrc.local"
 
 if ! grep ".bashrc.defaults" ~/.bashrc > /dev/null; then
   echo "source ~/.bashrc.defaults" >> ~/.bashrc
