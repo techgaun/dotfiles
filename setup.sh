d0348e8e02e1f4474bf6a02ca8e2b13e81c4a596
techgaun <coolsamar207@gmail.com>
2017-01-19 18:25:40 -0600

    add httpie config

--- setup.sh
+++ setup.sh
@@ -8,12 +8,15 @@ if [[ ! -d "${HOME}/.spf13-vim-3" || "x${FORCE_VIMINSTALL}" != "x" ]]; then
     curl -L -o - https://github.com/techgaun/spf13-vim/raw/3.0/bootstrap.sh | sh
 fi
 
+[[ ! -e "${HOME}/.httpie" ]] && mkdir "${HOME}/.httpie"
+
 ln -sf "${SRC_DIR}/.ackrc" "${HOME}/.ackrc"
 ln -sf "${SRC_DIR}/.bash_aliases" "${HOME}/.bash_aliases"
 ln -sf "${SRC_DIR}/.bash_prompt" "${HOME}/.bash_prompt"
 ln -sf "${SRC_DIR}/.bashrc.defaults" "${HOME}/.bashrc.defaults"
 ln -sf "${SRC_DIR}/.gitconfig" "${HOME}/.gitconfig"
 ln -sf "${SRC_DIR}/.gitignore" "${HOME}/.gitignore"
+ln -sf "${SRC_DIR}/httpie/config.json" "${HOME}/.httpie/config.json"
 ln -sf "${SRC_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
 ln -sf "${SRC_DIR}/.vimrc.bundles.local" "${HOME}/.vimrc.bundles.local"
 ln -sf "${SRC_DIR}/.vimrc.local" "${HOME}/.vimrc.local"
