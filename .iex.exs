05a0f69407a2f53ca7f68b6de4bdb1e492a92527
techgaun <coolsamar207@gmail.com>
2017-02-22 23:57:38 -0600

    customize elixir startup

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,22 @@
+IO.puts """
+        `
+       ;:
+      `+;
+      ++',
+     .+++,
+     :'++'.
+    .+'++';       Welcome to elixir-land
+    ,+'+''':
+   `:+'+';';;     http://elixir-lang.org
+   ,:++';;;;'.    http://www.phoenixframework.org/
+   ::'+';;;;''    https://hex.pm/
+   :;;++;;';''    http://www.elixirschool.com/
+   .;;'''';;;:    https://github.com/christopheradams/elixir_style_guide
+    ,:::'';,,.
+    ,,,,,:::,
+     ,,,,,,,
+      ``.`
+"""
 IEx.configure(
   alive_prompt: "%prefix(%node):%counter>",
   default_prompt: "%prefix:%counter>",
