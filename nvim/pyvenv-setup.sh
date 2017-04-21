02a893044ddcf95c666fced68bf3f0d1877ad606
techgaun <coolsamar207@gmail.com>
2017-04-21 14:53:23 -0500

    fix shellcheck

--- nvim/pyvenv-setup.sh
+++ nvim/pyvenv-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pyvenv-3.5 --clear --system-site-packages --copies ~/pyvenvs/neovim
+# shellcheck source=/dev/null
 source ~/pyvenvs/neovim/bin/activate
 pip install wheel
 pip install --upgrade pip
