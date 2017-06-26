840afd23e1178d98a4026025eb9ecd0150427866
techgaun <coolsamar207@gmail.com>
2017-06-26 16:55:05 -0500

    play around with tmux-resurrect

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ fi
 [[ -d "${NVIM_DIR}/autoload" ]] || curl -fLo "${NVIM_DIR}/autoload/plug.vim" --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+[[ -d "${HOME}/.tmux/plugins/tpm" ]] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ln -sf "${SRC_DIR}/.ackrc" "${HOME}/.ackrc"
 ln -sf "${SRC_DIR}/.agignore" "${HOME}/.agignore"
 ln -sf "${SRC_DIR}/.bash_aliases" "${HOME}/.bash_aliases"
