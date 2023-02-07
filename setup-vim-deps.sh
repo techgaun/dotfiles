f88b304c5e57ea791dabc40c3acdda10632cb227
techgaun <coolsamar207@gmail.com>
2023-02-06 22:46:51 -0600

    some additional cleanup

---
+++ setup-vim-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm i -g dockerfile-language-server-nodejs
+npm i -g bash-language-server
+npm i -g javascript-typescript-langserver
+pip3 install --user python-language-server
+npm i -g vue-language-server
+npm i -g vscode-css-languageserver-bin
+npm i -g vim-language-server
